bfbcff1dde6022cea42742db887a53533293f586
Aurelien Pillevesse <aupilleves@dpt_info.iut.local>
2017-05-29 09:38:41 +0200

    add first step classes

---
+++ src/AppBundle/Entity/JokePost.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * JokePost
+ */
+class JokePost
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $img;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return JokePost
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set img
+     *
+     * @param string $img
+     *
+     * @return JokePost
+     */
+    public function setImg($img)
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    /**
+     * Get img
+     *
+     * @return string
+     */
+    public function getImg()
+    {
+        return $this->img;
+    }
+}
+
---
+++ src/AppBundle/Entity/Vote.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Vote
+ */
+class Vote
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var int
+     */
+    private $choice;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set choice
+     *
+     * @param integer $choice
+     *
+     * @return Vote
+     */
+    public function setChoice($choice)
+    {
+        $this->choice = $choice;
+
+        return $this;
+    }
+
+    /**
+     * Get choice
+     *
+     * @return int
+     */
+    public function getChoice()
+    {
+        return $this->choice;
+    }
+}
+
