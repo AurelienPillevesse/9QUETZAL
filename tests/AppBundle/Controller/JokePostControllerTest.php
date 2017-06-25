b1d4a58c86ce0d3b0ab839ee81222f44510a8593
pipic1 <pierre.pic@etu.udamail.fr>
2017-06-25 13:17:17 +0200

    Test architecture

---
+++ tests/AppBundle/Controller/JokePostControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use AppBundle\Controller\JokePostController;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class JokePostControllerTest extends WebTestCase
+{
+  public function testLikeAction()
+  {
+      $jokepost = new JokePostController();
+      $result = $jokepost->likeAction();
+      $this->assertEquals(true,$client->getResponse()->isRedirect('/jokepost-one'));
+  }
+}
